a2a73147c6db240dcb373f800a6054ed37befd08
prince  katiyar <100132888+prince1katiyar@users.noreply.github.com>
2025-04-28 15:43:28 +0530

    Update setup.py
    updated

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "End-to-End-Machine-Learning-Pipeline"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "princekatiyar"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "princekatiyar986@gmail.com"
 
 
 setuptools.setup(
